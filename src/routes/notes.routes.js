03d8f67ca2c6b9015f4d2187f2065b053d7d0e77
shahzadiftikhar <shahzadiftikhar@outlook.com>
2026-01-04 03:08:23 +0800

    feat: add DELETE /api/v1/notes/:id API for deleting notes by ID

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { create, getAll, getById } = require('../controllers/notes.controller');
+const { create, getAll, getById, deleteNote } = require('../controllers/notes.controller');
 const { authenticate } = require('../middlewares/auth.middleware');
 const { validateNote } = require('../middlewares/validation.middleware');
 
 router.post('/', authenticate, validateNote, create);
 router.get('/', authenticate, getAll);
 router.get('/:id', authenticate, getById);
+router.delete('/:id', authenticate, deleteNote);
 
 module.exports = router;
